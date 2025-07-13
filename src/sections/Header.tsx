f2c23d10a52c8ca917a496a92b80854d4a622d44
Nadim Metwalli <nadimmetwalli@gmail.com>
2025-07-13 22:43:08 +0300

    Initial commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,12 @@
+import ArrowIcon from '@/assets/arrow-right.svg';
+import ftblcrewlogo from '@assets/theftblcrew-logo.png';
+import Image from 'next/image';
+import MenuIcon from '@/assets/menu.svg';
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <h1>This is a test</h1>
+    )
+
+}
